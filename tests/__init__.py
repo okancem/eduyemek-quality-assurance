e4b9c9a7f5ddc8b56b9348f70857c61c35a0f9a1
Okan Cem <67761306+okancem@users.noreply.github.com>
2025-12-24 17:09:14 -0500

    Delete tests directory

--- tests/__init__.py
+++
@@ -1,1 +0,0 @@
-# Tests package for Eduyemek QA project
--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from selenium import webdriver
-
-@pytest.fixture()
-
-def setup():
-    driver = webdriver.Chrome()
-    return driver
