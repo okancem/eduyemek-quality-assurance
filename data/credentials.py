fc73b5cff3803e97c28025f49afb096bcfadab8c
Okan Cem <okancem1122@gmail.com>
2026-01-15 16:50:40 -0500

    Stop tracking credentials and ignore secrets

--- data/credentials.py
+++
@@ -1,9 +0,0 @@
-VALID_USER = {
-    'email': 'okan@itu.edu.tr',
-    'password': 'nakomec112233'
-}
-
-INVALID_USERS = [
-    {'email': 'okan@itu.edu.tr', 'password': 'wrongpass'},
-    {'email': 'invalid@itu.edu.tr', 'password': 'password'}
-]
